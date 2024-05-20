78f95247e0e3dfddaff32dcb9047eac4c2b5788e
Yeasin1st <ysbd111@gmail.com>
2024-05-20 21:38:37 +0600

    prettier added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express"
-const app = express()
+import express, { Request, Response } from 'express';
+const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function main() {
-	try {
-		await mongoose.connect(config.db_url as string);
+  try {
+    await mongoose.connect(config.db_url as string);
 
-		app.listen(config.port, () => {
-			console.log(`Example app listening on port ${config.port}`)
-		})
-	} catch (error) {
-		console.log("Error: " + error);
-	}
-
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log('Error: ' + error);
+  }
 }
 
 main();
